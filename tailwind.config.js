28578aa207c200ffd9661683f4bff760804135f0
MomonSama <88752915+MomonSama@users.noreply.github.com>
2025-04-18 20:33:39 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  class="dark", // Enable dark mode using class
   plugins: [require('@midudev/tailwind-animations')],
 };
